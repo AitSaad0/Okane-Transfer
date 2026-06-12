23b620c2ae12095642969a2a94a90e416ffa5684
smahbblm <190425159+smahbblm@users.noreply.github.com>
2026-06-12 20:46:23 +0100

    chore: retirer widget_history.png et scripts start-frontend du tracking

--- start-frontend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "D:\BOULMANE\SMAHAN BOULMANE\TESTS-APP-OKAN-TREANSFERT\v1\front"
-start /B npx ng serve --port 4200 --proxy-config proxy.conf.json > ng-serve.log 2>&1
--- start-frontend.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location -LiteralPath "D:\BOULMANE\SMAHAN BOULMANE\TESTS-APP-OKAN-TREANSFERT\v1\front"
-npx ng serve --port 4200 --proxy-config proxy.conf.json 2>&1 | Out-File -LiteralPath "D:\BOULMANE\SMAHAN BOULMANE\TESTS-APP-OKAN-TREANSFERT\v1\front\ng-serve.log" -Encoding UTF8
